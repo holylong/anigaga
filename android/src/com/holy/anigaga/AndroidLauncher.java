5b1707b780eeb1058b2f6dfd44d41a7f94f31c15
holylong <mrhlingchen@163.com>
2020-12-11 14:04:50 +0800

    init

---
+++ android/src/com/holy/anigaga/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.holy.anigaga;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.holy.anigaga.AnigagaRender;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AnigagaRender(), config);
+	}
+}
---
+++ desktop/src/com/holy/anigaga/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.holy.anigaga.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.holy.anigaga.AnigagaRender;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AnigagaRender(), config);
+	}
+}
